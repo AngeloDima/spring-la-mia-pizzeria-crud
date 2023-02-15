b2613a38454175f9931e2c430098fa6356162f8d
AngeloDima <Angelodimauro91@gmail.com>
2023-02-15 15:07:11 +0100

    aggiungo i filtri (lato server)

--- src/main/java/com/example/demo/Controller/pizzeriaController.java
+++ src/main/java/com/example/demo/Controller/pizzeriaController.java
@@ -1,6 +1,8 @@
 package com.example.demo.Controller;
 
+
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Model.pizzeriaModel;
 import com.example.demo.Repository.pizzeriaRepository;
-
 
 
 @Controller
@@ -25,13 +27,32 @@ public class pizzeriaController {
 	pizzeriaRepository repository;
 	
 
-    @GetMapping
-    public String index(Model model) {
-        List<pizzeriaModel> elencoPizze = repository.findAll();
-		
-        model.addAttribute("elencoPizze", elencoPizze);
-        return "home";
-    }
+	
+	
+	
+	
+	@GetMapping
+	public String index(@RequestParam(name="keyword", required = false) String keyword, Double prezzo, Model model) {
+	    List<pizzeriaModel> elencoPizze;
+	    
+	    if (keyword != null && !keyword.isEmpty()) {
+	        elencoPizze = repository.findByNomeLike("%" + keyword + "%");
+	    } else {
+	        elencoPizze = repository.findAll();
+	    }
+	    
+	    if (prezzo != null) {
+	        elencoPizze = repository.findByPrezzo(prezzo);
+	    }
+	    
+	    model.addAttribute("elencoPizze", elencoPizze);
+	    return "home";
+	}
+    
+    
+    
+    
+    
     
     @GetMapping("pizza/{id}")
     public String detail(@PathVariable("id") Integer id, Model model) {
