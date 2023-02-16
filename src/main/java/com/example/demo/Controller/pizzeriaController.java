5c046b8ebca0614e582c00475fe8dbd311fe262f
AngeloDima <Angelodimauro91@gmail.com>
2023-02-16 13:01:05 +0100

    cambio lo style (da finire) aggiungo link per creare una pizza,  creo il metodo per creare una pizza e mandare il tutto sia al db che alla Home

--- src/main/java/com/example/demo/Controller/pizzeriaController.java
+++ src/main/java/com/example/demo/Controller/pizzeriaController.java
@@ -1,58 +1,63 @@
 package com.example.demo.Controller;
 
-
 import java.util.List;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.Model.pizzeriaModel;
 import com.example.demo.Repository.pizzeriaRepository;
 
-
 @Controller
 @RequestMapping("/")
 public class pizzeriaController {
 
-    
-    
+    @Autowired
+    pizzeriaRepository repository;
 
-	@Autowired
-	pizzeriaRepository repository;
-	
+    @GetMapping
+    public String index(@RequestParam(name = "keyword", required = false) String keyword,
+            @RequestParam(name = "prezzo", required = false) Double prezzo, Model model) {
+        List<pizzeriaModel> elencoPizze;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            elencoPizze = repository.findByNomeLike("%" + keyword + "%");
+        } else if (prezzo != null) {
+            elencoPizze = repository.findByPrezzo(prezzo);
+        } else {
+            elencoPizze = repository.findAll();
+        }
+
+        model.addAttribute("elencoPizze", elencoPizze);
+        return "home";
+    }
+
+    
+    
+    //crea
+    
+    @GetMapping("/create")
+    public String create(Model model) {
+        pizzeriaModel pizzeria = new pizzeriaModel();
+        model.addAttribute("pizzeria", pizzeria);
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String store(@ModelAttribute("pizzeria") pizzeriaModel formPizzeria, Model model) {
+        repository.save(formPizzeria);
+        return "redirect:/";
+    }
+
 
 	
-	
-	
-	
-	@GetMapping
-	public String index(@RequestParam(name="keyword", required = false) String keyword, Double prezzo, Model model) {
-	    List<pizzeriaModel> elencoPizze;
-	    
-	    if (keyword != null && !keyword.isEmpty()) {
-	        elencoPizze = repository.findByNomeLike("%" + keyword + "%");
-	    } else {
-	        elencoPizze = repository.findAll();
-	    }
-	    
-	    if (prezzo != null) {
-	        elencoPizze = repository.findByPrezzo(prezzo);
-	    }
-	    
-	    model.addAttribute("elencoPizze", elencoPizze);
-	    return "home";
-	}
-    
-    
-    
-    
-    
     
     @GetMapping("pizza/{id}")
     public String detail(@PathVariable("id") Integer id, Model model) {
